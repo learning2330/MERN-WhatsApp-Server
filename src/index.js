632dc87ef766ce22e75dac28c3c9fb03d796b4cb
learning2330 <learning.test2330@gmail.com>
2023-11-08 23:40:55 +0500

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
+import Logger from "./configs/logger.js";
 
 // env variable
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}...`);
+  Logger.info(`Server is listening at ${PORT}...`);
 });
