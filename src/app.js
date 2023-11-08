8bafec63192cbc820405b1abd5cf60fd4a085e37
learning2330 <learning.test2330@gmail.com>
2023-11-08 23:10:43 +0500

    add middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,54 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+// dotenv config
+dotenv.config();
 
 // Create express app
 const app = express();
+
+// Morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+// Helmet
+app.use(helmet());
+
+// Parse json request body
+app.use(express.json());
+
+// Parse json request url
+app.use(express.urlencoded({ extended: true }));
+
+// Sentize request data
+app.use(ExpressMongoSanitize());
+
+// Enable cookie paeser
+app.use(cookieParser());
+
+// Gzip comression
+app.use(compression());
+
+// File upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Cors
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello Whatsapp");
+});
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import dotenv from "dotenv";
 import app from "./app.js";
-
-// dotenv config
-dotenv.config();
 
 // env variable
 const PORT = process.env.PORT || 8000;
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}`);
+  console.log(`Server is listening at ${PORT}...`);
 });
